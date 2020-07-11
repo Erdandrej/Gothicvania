a61e42d3c9bb90b46289e6e2833b2ab3c7921f2d
Andrej Erdelsky <A.Erdelsky@student.tudelft.nl>
2020-07-11 15:36:33 +0200

    Add attack state

--- Gothicvania/Player/Player.gd
+++ Gothicvania/Player/Player.gd
@@ -8,13 +8,34 @@ export var AIR_RESISTANCE = 1
 export var GRAVITY = 4
 export var JUMP_FORCE = 140
 
+enum {
+	MOVE,
+	ATTACK,
+	CROUCH,
+	DODGE
+}
+
+var state = MOVE
 var motion = Vector2.ZERO
 
 onready var sprite = $Sprite
-onready var collisionShape = $CollisionShape2D
+onready var shape = $Shape
 onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		CROUCH:
+			pass
+		DODGE:
+			pass
+	
+func move_state(delta):
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if x_input != 0:
@@ -36,7 +57,10 @@ func _physics_process(delta):
 			motion.y = -JUMP_FORCE
 
 	else:
-		animationPlayer.play("Jump")
+		if Input.is_action_pressed("attack"):
+			animationPlayer.play("JumpKick")
+		else:
+			animationPlayer.play("Jump")
 			
 		if Input.is_action_just_released("jump") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2
@@ -44,4 +68,20 @@ func _physics_process(delta):
 		if x_input == 0:
 			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
 			
+	move(motion)
+
+func attack_state(delta):
+	if is_on_floor():
+		if Input.get_action_strength("ui_down") == 1:
+			animationPlayer.play("CrouchKick")
+		elif Input.get_action_strength("ui_up") == 1:
+			animationPlayer.play("Kick")
+		else:
+			animationPlayer.play("Punch")
+	
+		yield(animationPlayer, "animation_finished")
+		
+	state = MOVE
+
+func move(motion):
 	motion = move_and_slide(motion, Vector2.UP)
